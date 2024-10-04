9e46f374c1901ffd6c00134a9429c2e715b070e8
JuanPabloMadrigal <a01571170@tec.mx>
2024-10-04 09:16:54 -0600

    Creacion de caminos

---
+++ Assets/Scripts/Main Game/Construcción/PlaceBuilding.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlaceBuilding : MonoBehaviour
+{
+    private GameObject buildingPreviewPrefab;
+    private GameObject buildingToBuild;
+    private GameObject newPreview;
+    private bool isPreviewActive = false;
+
+    private GameObject[] buildingButtons;
+
+    public void Start()
+    {
+        buildingButtons = GameObject.FindGameObjectsWithTag("BuildingButton");
+    }
+
+
+    public void BeginBuildingPreview(int buildingOption)
+    {
+        switch (buildingOption)
+        {
+            case 0:
+                buildingPreviewPrefab = Resources.Load<GameObject>("Prefabs/Edificios/Horno1HoverPrefab");
+                buildingToBuild = Resources.Load<GameObject>("Prefabs/Edificios/Horno 1");
+                newPreview = Instantiate(buildingPreviewPrefab, new Vector3(0f + buildingPreviewPrefab.transform.position.x, 0f + buildingPreviewPrefab.transform.position.y, 0f + buildingPreviewPrefab.transform.position.z), Quaternion.identity);
+                isPreviewActive = true;
+
+                foreach(GameObject button in buildingButtons)
+                {
+                    button.GetComponent<Button>().interactable = false;
+                }
+
+                break;
+            case 1:
+                buildingPreviewPrefab = Resources.Load<GameObject>("Prefabs/Edificios/AlmacenMateriaPrimaHoverPrefab");
+                buildingToBuild = Resources.Load<GameObject>("Prefabs/Edificios/Almacén de Materia Prima");
+                newPreview = Instantiate(buildingPreviewPrefab, new Vector3(0f + buildingPreviewPrefab.transform.position.x, 0f + buildingPreviewPrefab.transform.position.y, 0f + buildingPreviewPrefab.transform.position.z), Quaternion.identity);
+                isPreviewActive = true;
+
+                foreach (GameObject button in buildingButtons)
+                {
+                    button.GetComponent<Button>().interactable = false;
+                }
+                break;
+        }
+        
+    }
+
+    public void OnClickKey(InputAction.CallbackContext ctx)
+    {
+        Debug.Log(isPreviewActive);
+        if (isPreviewActive)
+        {
+            Debug.Log("previewactive");
+            bool canBuild = newPreview.GetComponent<PlacingTest>().canBuild;
+            bool clickKeyPressed = ctx.started;
+            if (clickKeyPressed && canBuild)
+            {
+                Debug.Log("Click Izq. Detectado");
+                Debug.Log(newPreview.transform.position);
+                GameObject placedBuilding = Instantiate(buildingToBuild, newPreview.transform.position, newPreview.transform.rotation);
+                isPreviewActive = false;
+                foreach (GameObject button in buildingButtons)
+                {
+                    button.GetComponent<Button>().interactable = true;
+                }
+                Destroy(newPreview);
+            }
+        }
+    }
+
+    public void OnQkey(InputAction.CallbackContext ctx)
+    {
+        if (isPreviewActive)
+        {
+            bool qKeyPressed = ctx.started;
+
+            if (qKeyPressed)
+            {
+                newPreview.transform.rotation = Quaternion.Euler(newPreview.transform.rotation.eulerAngles.x, newPreview.transform.rotation.eulerAngles.y - 90f, newPreview.transform.rotation.eulerAngles.z);
+            }
+        }
+    }
+
+    public void OnEkey(InputAction.CallbackContext ctx)
+    {
+        if (isPreviewActive)
+        {
+            bool eKeyPressed = ctx.started;
+
+            if (eKeyPressed)
+            {
+                newPreview.transform.rotation = Quaternion.Euler(newPreview.transform.rotation.eulerAngles.x, newPreview.transform.rotation.eulerAngles.y + 90f, newPreview.transform.rotation.eulerAngles.z);
+            }
+        }
+    }
+}
--- Assets/Scripts/Main Game/Construcción/PlacingTest.cs
+++ Assets/Scripts/Main Game/Construcción/PlacingTest.cs
@@ -15,13 +15,23 @@ public class PlacingTest : MonoBehaviour
     private GameObject logic;
     private PlacingWithModelTest buildBuildingScript;
 
+    private float original_x;
+    private float original_y;
+    private float original_z;
+
     // Start is called before the first frame update
     void Start()
     {
         canBuildBuilding.SetActive(true);
         cannotBuildBuilding.SetActive(false);
+<<<<<<< Updated upstream
         logic = GameObject.Find("Logic");
         buildBuildingScript = logic.GetComponent<PlacingWithModelTest>();
+=======
+        original_x = transform.position.x;
+        original_y = transform.position.y;
+        original_z = transform.position.z;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -32,7 +42,7 @@ public class PlacingTest : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
         {
-            Vector3 targetPosition = new Vector3((float)(int)hit.point.x, 0, (float)(int)hit.point.z) + Vector3.up * heightAboveGround;
+            Vector3 targetPosition = new Vector3((float)(int)hit.point.x + original_x, 0 + original_y, (float)(int)hit.point.z + original_z) + Vector3.up * heightAboveGround;
             transform.position = targetPosition;
         }
 
