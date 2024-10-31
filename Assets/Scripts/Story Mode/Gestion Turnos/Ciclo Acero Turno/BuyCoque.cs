d33cc35e11f94c60a90497663f5b2668aacea45b
valdezae <80776085+valdezae@users.noreply.github.com>
2024-10-31 11:16:40 -0600

    working buttons

--- Assets/Scripts/Story Mode/Gestion Turnos/Ciclo Acero Turno/BuyCoque.cs
+++ Assets/Scripts/Story Mode/Gestion Turnos/Ciclo Acero Turno/BuyCoque.cs
@@ -6,12 +6,30 @@ using TMPro;
 public class BuyCoque : MonoBehaviour
 {
     [SerializeField] private TMP_InputField quantityInputField;
-
+    public int coqueToBuy = 0;
 
     public void buyCoque()
     {
         FileHandlerStory.Instance.gameData.AddCoque(int.Parse(quantityInputField.text));
         FileHandlerStory.Instance.gameData.SubtractMoney(FileHandlerStory.Instance.gameData.coquePrice);
         Debug.Log(FileHandlerStory.Instance.gameData.coquePrice);
+    }
+
+    public void addCoque()
+    {
+        coqueToBuy = int.Parse(quantityInputField.text);
+        coqueToBuy++;
+        quantityInputField.text = coqueToBuy.ToString();
+    }
+
+    public void subCoque()
+    {
+        if(coqueToBuy == 0)
+        {
+            return; 
+        }
+        coqueToBuy = int.Parse(quantityInputField.text);
+        coqueToBuy--;
+        quantityInputField.text = coqueToBuy.ToString();
     }
 }
