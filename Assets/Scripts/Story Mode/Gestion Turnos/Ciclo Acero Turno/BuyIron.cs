0bda9078717918b8e0d8e4f67be72fc49cea76f0
Erick Valdez <A00829909@itesm.mx>
2024-11-04 00:31:43 -0600

    horno 1 ui

--- Assets/Scripts/Story Mode/Gestion Turnos/Ciclo Acero Turno/BuyIron.cs
+++ Assets/Scripts/Story Mode/Gestion Turnos/Ciclo Acero Turno/BuyIron.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class BuyIron : MonoBehaviour
 {
     [SerializeField] public TMP_InputField quantityInputField;
+    public int ironToBuy = 0;
 
 
     public void buyIron()
@@ -13,5 +14,23 @@ public class BuyIron : MonoBehaviour
         FileHandlerStory.Instance.gameData.AddIron(int.Parse(quantityInputField.text));
         FileHandlerStory.Instance.gameData.SubtractMoney(FileHandlerStory.Instance.gameData.ironPrice);
         Debug.Log(FileHandlerStory.Instance.gameData.ironPrice);
+    }
+
+    public void addIron()
+    {
+        ironToBuy = int.Parse(quantityInputField.text);
+        ironToBuy++;
+        quantityInputField.text = ironToBuy.ToString();
+    }
+
+    public void subIron()
+    {
+        if (ironToBuy == 0)
+        {
+            return;
+        }
+        ironToBuy = int.Parse(quantityInputField.text);
+        ironToBuy--;
+        quantityInputField.text = ironToBuy.ToString();
     }
 }
