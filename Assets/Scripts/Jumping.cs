521ea1b94e85418c9cd69d089d654620907e0457
RaulKenyu <a01236331@tec.mx>
2024-11-06 17:19:39 -0600

    NPC animation details

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Jumping : MonoBehaviour
 {
-    public float amplitude = 1.0f;
+    public float amplitude1 = 0.3f;
+    public float amplitude2 = 0.02f;
     public float frequency = 0.1f;
+    public float defaultScale = 0.05f;
     [SerializeField]Vector3 posOrigin = new Vector3();
     Vector3 tempPos = new Vector3();
     public GameObject fatherAgent;
@@ -20,7 +22,10 @@ public class Jumping : MonoBehaviour
     void Update()
     {
         tempPos = new Vector3(fatherAgent.transform.position.x, posOrigin.y, fatherAgent.transform.position.z);
-        tempPos.y = amplitude * Mathf.Sin((float)Time.fixedTime * (float)Mathf.PI * frequency) + .3f;
+        tempPos.y = amplitude1 * Mathf.Sin((float)Time.fixedTime * (float)Mathf.PI * frequency) + .3f;
         transform.position = tempPos;
+        transform.localScale = new Vector3((-Mathf.Sin((float)Time.fixedTime * (float)Mathf.PI * frequency)* amplitude2 + defaultScale), 
+            (Mathf.Sin((float)Time.fixedTime * (float)Mathf.PI * frequency)* amplitude2 + defaultScale), 
+            (-Mathf.Sin((float)Time.fixedTime * (float)Mathf.PI * frequency)* amplitude2 + defaultScale));
     }
 }
