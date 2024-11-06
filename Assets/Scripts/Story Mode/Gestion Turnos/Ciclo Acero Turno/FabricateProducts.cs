100a9a551b9f74d619b59aa6433f7feddf57ac06
valdezae <80776085+valdezae@users.noreply.github.com>
2024-11-06 12:36:12 -0600

    working product fabrication

--- Assets/Scripts/Story Mode/Gestion Turnos/Ciclo Acero Turno/FabricateProducts.cs
+++ Assets/Scripts/Story Mode/Gestion Turnos/Ciclo Acero Turno/FabricateProducts.cs
@@ -5,7 +5,9 @@ using TMPro;
 
 public class FabricateProducts : MonoBehaviour
 {
-    [SerializeField] public TMP_InputField quantityInputField;
+    [SerializeField] public TMP_InputField quantityInputFieldAcero;
+    [SerializeField] public TMP_Text steelCostUI;
+
     public int productToBuy = 0;
 
     public void fabricateProduct(string product)
@@ -13,10 +15,53 @@ public class FabricateProducts : MonoBehaviour
         switch (product)
         {
             case "acero":
-                FileHandlerStory.Instance.gameData.AddSteel(int.Parse(quantityInputField.text));
+                FileHandlerStory.Instance.gameData.AddSteel(int.Parse(quantityInputFieldAcero.text));
                 FileHandlerStory.Instance.gameData.SubtractIron(FileHandlerStory.Instance.gameData.steelIronPrice*productToBuy);
+                //TODO: Add removeCoque to model and add it here
                 break;
             
         }
+    }
+    
+    public void updateCostUI(string product)
+    {
+        switch (product)
+        {
+            case "acero":
+                var ironCost = FileHandlerStory.Instance.gameData.steelIronPrice * int.Parse(quantityInputFieldAcero.text);
+                var coqueCost = 2 * int.Parse(quantityInputFieldAcero.text);
+                steelCostUI.text = $"Hierro: {ironCost} \n Coque: {coqueCost}";
+                break;
+        }
+    }
+
+    public void addProduct(string product)
+    {
+        switch (product)
+        {
+            case "acero":
+                productToBuy = int.Parse(quantityInputFieldAcero.text);
+                productToBuy++;
+                quantityInputFieldAcero.text = productToBuy.ToString();
+                break;
+        }
+        
+    }
+
+    public void subProduct(string product)
+    {
+        switch (product)
+        {
+            case "acero":
+                if (productToBuy == 0)
+                {
+                    return;
+                }
+                productToBuy = int.Parse(quantityInputFieldAcero.text);
+                productToBuy--;
+                quantityInputFieldAcero.text = productToBuy.ToString();
+                break;
+        }
+        
     }
 }
